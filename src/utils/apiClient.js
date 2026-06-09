8823ea4faa09bda34c23757a4a54bef6750dd28e
anisakhmedov <akhmedovanis5@gmail.com>
2026-06-09 15:53:53 +0500

    asd

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -6,7 +6,7 @@ import {
   saveUserToStorage,
 } from './storageUtils';
 
-export const API_URL = import.meta.env.VITE_API_URL || 'https://siut-internships-5e35adaf79be.herokuapp.com';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://siut-54236c4acf55.herokuapp.com';
 
 const AUTH_SESSION_EXPIRED_EVENT = 'auth:session-expired';
 
